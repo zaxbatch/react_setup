0d297625a300f8595246968a3a730edf7dc1956d
zax <sneakjunkmail@gmail.com>
2021-07-14 22:00:18 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import App from './containers/App';
+import reportWebVitals from './containers/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
